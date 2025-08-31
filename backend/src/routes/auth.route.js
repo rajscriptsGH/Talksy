547b369ad9f33f7b191e3a71c8493b653cd9f05c
rajyadav63 <vector36razz@gmail.com>
2025-08-31 17:08:50 +0530

    db connection

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -4,9 +4,11 @@ import { signup, login, logout } from '../controllers/auth.controlller.js';
 
 const router = express.Router();
 
-router.get("/signup", signup)
-router.get("/login", login)
-router.get("/logout", logout)
+router.post("/signup", signup)
+
+router.post("/login", login)
+
+router.post("/logout", logout)
 
 
 export default router;
