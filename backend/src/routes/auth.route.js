561a89f2c3d952a22c43a3fb7412be931aef6526
rajyadav63 <vector36razz@gmail.com>
2025-08-31 16:53:24 +0530

    controller added

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,17 +1,12 @@
 
 import express from 'express'
+import { signup, login, logout } from '../controllers/auth.controlller.js';
 
 const router = express.Router();
 
-router.get("/signup", (req, res) => {
-    res.send("Signup route")
-})
-router.get("/login", (req, res) => {
-    res.send("Login route")
-})
-router.get("/logout", (req, res) => {
-    res.send("Logout route")
-})
+router.get("/signup", signup)
+router.get("/login", login)
+router.get("/logout", logout)
 
 
 export default router;
