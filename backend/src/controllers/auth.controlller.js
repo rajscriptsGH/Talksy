d67c553ac1081191fe77b98a9468e302a7b1202a
rajyadav63 <vector36razz@gmail.com>
2025-09-01 12:16:08 +0530

    check existing user email

--- backend/src/controllers/auth.controlller.js
+++ backend/src/controllers/auth.controlller.js
@@ -1,7 +1,40 @@
+import User from "../models/User.js";
+
+export async function signup(req, res) {
+    const { fullName, email, password } = req.body
+
+    try {
+        if (!fullName || !email || !password) {
+            return res.status(400), json({
+                message: "All fields are requires"
+            })
+        }
+
+        if (password.length < 4) {
+            return res.status(400), json({
+                message: "Password length must be atleast 4 characters"
+            })
+        }
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            return res.status(400), json({
+                message: "Invalid email"
+            })
+        }
+
+        const existingUser = await User.findOne({ email })
+        if (existingUser) {
+            return res.status(400).json({
+                message: "Email already exists"
+            })
+        }
+
+        
 
 
-export function signup(req, res) {
-    res.send("Signup Route")
+    } catch (error) {
+
+    }
 }
 export function login(req, res) {
     res.send("login Route")
