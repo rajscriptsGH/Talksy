7d36eb992e5e4341abb26d09ddab1938ecbf2711
rajyadav63 <vector36razz@gmail.com>
2025-09-02 12:31:03 +0530

    steam user error resolved

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -1,6 +1,6 @@
 import { StreamChat } from 'stream-chat'
 import "dotenv/config"
-import User from '../models/User'
+import User from '../models/User.js'
 
 const apiKey = process.env.STREAM_API_KEY
 const apiSecret = process.env.STREAM_API_SECRET
