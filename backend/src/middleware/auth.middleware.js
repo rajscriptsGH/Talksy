ac17ca587c180b30bb4b107ebc64db25c954367f
rajyadav63 <vector36razz@gmail.com>
2025-09-03 09:59:10 +0530

    middleware added for unboarding

---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,30 @@
+
+import jwt from 'jsonwebtoken'
+import User from '../models/User.js'
+
+export const middlewareAuthRoute = async (req, res, next) => {
+    const token = req.cookies.jwt;
+    console.log("token", token);
+
+    if (!token) {
+        return res.status(401).json({ message: "Unauthorized-token not found" });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        if (!decoded.userId) {
+            return res.status(401).json({ message: "Unauthorized-user not found" });
+        }
+
+        const user = await User.findById(decoded.userId).select("-password");
+        if (!user) {
+            return res.status(401).json({ message: "Unauthorized-user not found" });
+        }
+
+        req.user = user;
+        next();
+
+    } catch (error) {
+        return res.status(401).json({ message: "Unauthorized-token not verified" });
+    }
+}
