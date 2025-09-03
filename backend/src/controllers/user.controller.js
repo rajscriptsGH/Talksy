9ef4fa833076849158a877012dd7ad3e9c796415
rajyadav63 <vector36razz@gmail.com>
2025-09-03 11:29:21 +0530

    getReccomendedUsers route

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,11 +1,28 @@
 
 
-
 export async function getReccomendedUsers(req, res) {
     try {
-       
+        const currentUserId = req.user.id;
+        const currentUser = req.user;
+
+        const reccomendedUsers = await User.find({
+            $and: [
+
+                { _id: { $ne: currentUserId } },
+                { $id: { $ne: currentUser.friends } },
+                { isOnBorded: true },
+            ]
+        })
+        res.status(200).json({
+            success: true,
+            users: reccomendedUsers
+        });
     } catch (error) {
         console.log("error in getReccomendedUsers", error);
+        res.status(500).json({
+            success: false,
+            message: "Internal server error"
+        });
     }
 }
 
