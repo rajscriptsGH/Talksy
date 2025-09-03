6c8614eb0fc37a708baec2e84e65eb3fa06743bb
rajyadav63 <vector36razz@gmail.com>
2025-09-03 12:52:52 +0530

    get notification endpoint

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -122,3 +122,24 @@ export async function acceptFriendRequest() {
         res.status(500).json({ message: "Server error" })
     }
 }
+
+//for notifications
+export async function getFriendRequests(req, res) {
+    try {
+        const incomingReqs = await FriendRequest.find({
+            recipient: req.user.id,
+            status: "pending",
+        }).populate("sender", "fullName profilePic nativeLanguage learningLanguage");
+
+        const acceptedReqs = await FriendRequest.find({
+            sender: req.user.id,
+            status: "accepted",
+        }).populate("recipient", "fullName profilePic");
+
+        res.status(200).json({ incomingReqs, acceptedReqs });
+    } catch (error) {
+        console.log("Error in getting notification", error.message);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+}
+
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { middlewareAuthRoute } from '../middleware/auth.middleware.js'
-import { getReccomendedUsers, getMyFriends, sendFriendRequest, acceptFriendRequest } from '../controllers/user.controller.js'
+import { getReccomendedUsers, getMyFriends, sendFriendRequest, acceptFriendRequest, getFriendRequests } from '../controllers/user.controller.js'
 
 const router = express.Router()
 
@@ -14,5 +14,6 @@ router.post("/friend-request/:id", sendFriendRequest)
 router.put("/friend-request/:id/accept", acceptFriendRequest)
 
 
+router.get("/friend-requests", getFriendRequests)
 
 export default router;
