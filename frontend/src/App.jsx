776c8313d1b718bd45368abaaba398450101f07c
rajyadav63 <vector36razz@gmail.com>
2025-09-04 10:42:32 +0530

    daisy ui added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,15 @@
 function App() {
 
   return (
-    <div>
-      <h1 className='bg-blue-700'>Hello from talksy</h1>
+    <div className="flex flex-col gap-3 items-center justify-center h-screen w-screen">
+      <button className="btn btn-soft">Default</button>
+      <button className="btn btn-soft btn-primary">Primary</button>
+      <button className="btn btn-soft btn-secondary">Secondary</button>
+      <button className="btn btn-soft btn-accent">Accent</button>
+      <button className="btn btn-soft btn-info">Info</button>
+      <button className="btn btn-soft btn-success">Success</button>
+      <button className="btn btn-soft btn-warning">Warning</button>
+      <button className="btn btn-soft btn-error">Error</button>
     </div>
   )
 }
