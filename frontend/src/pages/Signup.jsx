2f2ba95bdf15f0db86bfa637ae431029f53d86ec
rajyadav63 <vector36razz@gmail.com>
2025-09-06 12:36:29 +0530

    illustration done in signup

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { FileVideoCamera } from 'lucide-react'
+import { Link, Navigate } from 'react-router'
 
 const Signup = () => {
     const [signupData, setSignupData] = useState({
@@ -7,6 +8,8 @@ const Signup = () => {
         email: "",
         password: ""
     })
+
+    
 
     const handleSignup = (e) => {
         e.preventDefault()
@@ -29,11 +32,90 @@ const Signup = () => {
                     {/* form */}
                     <div className='w-full px-5'>
                         <form onSubmit={handleSignup}>
-                           
+                            <div className='mt-2 mb-5'>
+                                <h1 className='text-[20px] font-medium'>Create Account</h1>
+                                <p className='text-[15px] font-light text-slate-400'>Join Talksy ans talk to people around the world</p>
+                            </div>
+
+                            <div className='space-y-5'>
+                                {/* fullName */}
+                                <div className='form-controll w-full'>
+                                    <label className='label'>
+                                        <span className='label-text text-white font-medium'>Full Name</span>
+                                    </label> <br />
+                                    <input
+                                        type="text"
+                                        placeholder='Razz'
+                                        value={signupData.fullName}
+                                        onChange={(e) =>
+                                            setSignupData({ ...signupData, fullName: e.target.value })}
+                                        required
+                                        className=' w-full border border-purple-300 rounded-lg px-5 py-2 outline-0'
+                                    />
+                                </div>
+                                {/* Email */}
+                                <div className='form-controll w-full'>
+                                    <label className='label'>
+                                        <span className='label-text text-white font-medium'>Email</span>
+                                    </label> <br />
+                                    <input
+                                        type="email"
+                                        placeholder='razz@gmail.com'
+                                        value={signupData.email}
+                                        onChange={(e) =>
+                                            setSignupData({ ...signupData, email: e.target.value })}
+                                        required
+                                        className='w-full border border-purple-300 rounded-lg px-5 py-2 outline-0'
+                                    />
+                                </div>
+                                {/* Password */}
+                                <div className='form-controll w-full'>
+                                    <label className='label'>
+                                        <span className='label-text text-white font-medium'>Password</span>
+                                    </label> <br />
+                                    <input
+                                        type="password"
+                                        placeholder='********'
+                                        value={signupData.password}
+                                        onChange={(e) =>
+                                            setSignupData({ ...signupData, password: e.target.value })}
+                                        required
+                                        className='w-full border border-purple-300 rounded-lg px-5 py-2 outline-0'
+                                    />
+                                    <p className='text-[10px] text-slate-400'>Passwoed must be at least 4 characters long</p>
+                                </div>
+                            </div>
+
+                            <button
+                                className='btn btn-primary mt-7 mb-2 w-full' type='submit'>Create Account
+                            </button>
+
+                            <p className='text-center mt-2'>
+                                Already have an accout?
+                                <Link
+                                    to={"/login"}
+                                    className='text-green-500 cursor-pointer hover:text-green-400 ml-2'>
+                                    Sign in
+                                </Link>
+                            </p>
                         </form>
                     </div>
 
 
+                </div>
+                {/* right side */}
+                <div className="hidden lg:flex lg:flex-col w-full lg:w-1/2 bg-primary/10 items-center justify-center py-5 px-2">
+                    <div className="max-w-md p-8">
+                        <div className="relative aspect-square max-w-sm mx-auto">
+                            <img src="/videocall.png" alt="Language connection illustration" className="w-full h-full" />
+                        </div>
+                    </div>
+                    <div className="text-center space-y-3 mt-6">
+                        <h2 className="text-2xl font-bold">Connect with people worldwide</h2>
+                        <p className="opacity-70">
+                            Make friends & practice conversations
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
