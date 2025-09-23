c2c985db377a52a88ccaedee8c2b914624019406
rajyadav63 <vector36razz@gmail.com>
2025-09-23 11:56:02 +0530

    notification routes corrected

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -47,8 +47,18 @@ function App() {
           element={!isAuthenticated ? <Login /> : <Navigate to={isOnboarded ? "/" : "/onboarding"} />} />
 
         <Route path='/chatpage' element={isAuthenticated ? <ChatPage /> : <Navigate to="/login" />} />
-        <Route path='/notifications' element={isAuthenticated ? <Notification /> : <Navigate to="/login" />} />
+
+        <Route path='/notifications'
+          element={isAuthenticated && isOnboarded ? (
+            <Layout showSidebar={true}>
+              <Notification />
+            </Layout>
+          ) : (
+            <Navigate to={isAuthenticated ? "/login" : "/onboarding"} />
+          )} />
+
         <Route path='/callpage' element={isAuthenticated ? <CallPage /> : <Navigate to="/login" />} />
+
         <Route
           path='/onboarding'
           element={isAuthenticated ? (
